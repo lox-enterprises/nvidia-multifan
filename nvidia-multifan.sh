729c4bba850e2411acb1a1d9aa56d055a4994413
kasimir <info@lox.enterprises>
2024-09-06 10:11:49 +0200

    Simplified nvidia-settings commands, added control display

--- nvidia-multifan.sh
+++ nvidia-multifan.sh
@@ -2,20 +2,19 @@
 
 # command
 setfan() {
-  sudo /usr/bin/nvidia-settings -a "*:1[gpu:0]/GPUFanControlState=1" -a "*:1[fan-0]/GPUTargetFanSpeed=$1" -a "*:1[fan-1]/GPUTargetFanSpeed=$2"
+  nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=$1 -a [fan:1]/GPUTargetFanSpeed=$2 -c 0
 }
 
 while true; do
     gpu_temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
 
-    # temperature categories
+    # temperature categories, setfan: first value is first fan, second value second fan
     if ((0<=gpu_temp && gpu_temp<=50))
         then
-        # set fan control back to GPU to allow for fan stop
-        sudo /usr/bin/nvidia-settings -a "*:1[gpu:0]/GPUFanControlState=0"
+        # fan stop
+        nvidia-settings -a [gpu:0]/GPUFanControlState=0 -c 0
     elif ((51<=gpu_temp && gpu_temp<=60))
         then
-        # setfan [speed first fan] [speed second fan]
         setfan 30 30
     elif ((61<=gpu_temp && gpu_temp<=100))
         then
